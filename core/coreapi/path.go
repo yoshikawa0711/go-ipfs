256d662075757e311d2f9c3d01e3083174315ec8
yoshikawa0711 <g2121060@fun.ac.jp>
2022-09-06 14:49:28 +0900

    add: パラメータ付きのリクエストが来た際に、オリジナルとリサイズ後の画像を並行して問い合わせる機能の実装

--- core/coreapi/path.go
+++ core/coreapi/path.go
@@ -3,10 +3,19 @@ package coreapi
 import (
 	"context"
 	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"os"
 	gopath "path"
+	"strconv"
+	"strings"
 
+	files "github.com/ipfs/go-ipfs-files"
 	"github.com/ipfs/go-namesys/resolve"
+	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipfs/kubo/tracing"
+	"golang.org/x/image/draw"
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
@@ -26,15 +35,82 @@ func (api *CoreAPI) ResolveNode(ctx context.Context, p path.Path) (ipld.Node, er
 	ctx, span := tracing.Span(ctx, "CoreAPI", "ResolveNode", trace.WithAttributes(attribute.String("path", p.String())))
 	defer span.End()
 
+	// split parameter
+	p, param := separateParameter(p)
+
 	rp, err := api.ResolvePath(ctx, p)
 	if err != nil {
 		return nil, err
 	}
 
-	node, err := api.dag.Get(ctx, rp.Cid())
-	if err != nil {
-		return nil, err
+	// organize parameter and set cid.param
+	c := rp.Cid()
+	if param != "" {
+		param, err = organizeParameter(param)
+		if err != nil {
+			return nil, err
+		}
+
+		c.SetParam(param)
 	}
+
+	// check and get resize cid <CID>&<Param>:<resized CID> table
+	_, newc := c.IsExistResizeCid()
+
+	var node ipld.Node
+	if newc.Defined() {
+		// if newc is not undef, get c and newc in parallel
+		nodech, errch := getOriginalAndResizeInParallel(ctx, api, c, newc)
+		errCount := 0
+
+	nodeWait:
+		for {
+			select {
+			case n := <-nodech:
+				node = n
+				break nodeWait
+			case _ = <-errch:
+				errCount += 1
+				if errCount >= 2 {
+					return nil, fmt.Errorf("searching node error: %s", c.StringWithParam())
+				}
+
+				continue
+			case <-ctx.Done():
+				return nil, fmt.Errorf("context timeout in searching node: %s", c.StringWithParam())
+			}
+		}
+	} else if c.GetParam() != "" {
+		// if c has parameter, get c with param and c without param in parallel
+		nodech, errch := getNodeParallel(ctx, api, c)
+		errCount := 0
+
+	nodeWait2:
+		for {
+			select {
+			case n := <-nodech:
+				node = n
+				break nodeWait2
+			case _ = <-errch:
+				errCount += 1
+				if errCount >= 2 {
+					return nil, fmt.Errorf("searching node error: %s", c.StringWithParam())
+				}
+
+				continue
+			case <-ctx.Done():
+				return nil, fmt.Errorf("context timeout in searching node: %s", c.StringWithParam())
+			}
+		}
+
+	} else {
+		// if newc is undef and c doesn't have parameter, simply get node
+		node, err = getNode(ctx, api, c)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return node, nil
 }
 
@@ -82,4 +158,275 @@ func (api *CoreAPI) ResolvePath(ctx context.Context, p path.Path) (path.Resolved
 	}
 
 	return path.NewResolvedPath(ipath, node, root, gopath.Join(rest...)), nil
+}
+
+func separateParameter(p path.Path) (path.Path, string) {
+	parts := strings.Split(p.String(), "&")
+	if len(parts) >= 2 {
+		return path.New(parts[0]), parts[1]
+	} else {
+		return path.New(parts[0]), ""
+	}
+}
+
+func organizeParameter(param string) (string, error) {
+	m, err := splitParameter(param)
+	if err != nil {
+		return "", err
+	}
+
+	var newparam string
+	if v, ok := m["w"]; ok {
+		newparam += "w=" + fmt.Sprint(v)
+	}
+
+	if v, ok := m["h"]; ok {
+		if newparam != "" {
+			newparam += ","
+		}
+		newparam += "h=" + fmt.Sprint(v)
+	}
+
+	return newparam, nil
+}
+
+func splitParameter(params string) (map[string]int, error) {
+	m := make(map[string]int)
+
+	parts := strings.Split(params, ",")
+
+	for _, v := range parts {
+		param := strings.Split(v, "=")
+
+		if len(param) != 2 {
+			return nil, fmt.Errorf("invalid parameter: %s", v)
+		}
+
+		if param[0] == "w" || param[0] == "h" {
+
+			if _, ok := m[param[0]]; !ok {
+				var err error
+
+				m[param[0]], err = strconv.Atoi(param[1])
+				if err != nil {
+					return nil, err
+				}
+
+			} else {
+				return nil, fmt.Errorf("invalid parameter: %s: already exist", param[0])
+			}
+
+		} else {
+			return nil, fmt.Errorf("invalid parameter: %s is not supported", param[0])
+		}
+	}
+
+	return m, nil
+
+}
+
+func createNewImageNode(ctx context.Context, api *CoreAPI, node ipld.Node, param string) (ipld.Node, error) {
+	m, err := splitParameter(param)
+	if err != nil {
+		return nil, err
+	}
+
+	ses := api.getSession(ctx)
+	f, err := unixfile.NewUnixfsFile(ctx, ses.dag, node)
+	if err != nil {
+		return nil, err
+	}
+
+	fn, err := transformImage(f, m)
+	if err != nil {
+		return nil, err
+	}
+
+	rp, err := api.Unixfs().Add(ctx, fn)
+	if err != nil {
+		return nil, err
+	}
+
+	newnode, err := api.dag.Get(ctx, rp.Cid())
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("new cid is " + rp.Cid().String())
+	saveLink("/ipfs/"+node.Cid().String()+"&"+param, "/ipfs/"+newnode.Cid().String())
+
+	return newnode, nil
+}
+
+func transformImage(fn files.Node, m map[string]int) (files.Node, error) {
+	f := files.ToFile(fn)
+
+	w, err := os.Create("resource.png")
+	_, err = io.Copy(w, f.(io.Reader))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		w.Close()
+		_ = os.Remove("resource.png")
+	}()
+
+	w, _ = os.Open("resource.png")
+	img, _, err := image.Decode(w)
+	if err != nil {
+		return nil, err
+	}
+
+	width, height := img.Bounds().Dx(), img.Bounds().Dy()
+	if v, ok := m["w"]; ok {
+		width = v
+	}
+
+	if v, ok := m["h"]; ok {
+		height = v
+	}
+
+	newimg := image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.BiLinear.Scale(newimg, newimg.Bounds(), img, img.Bounds(), draw.Over, nil)
+
+	output, err := os.Create("transformed.png")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove("transformed.png")
+
+	err = png.Encode(output, newimg)
+	if err != nil {
+		return nil, err
+	}
+	output.Close()
+
+	output, _ = os.Open("transformed.png")
+	stat, err := output.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	outnode, err := files.NewReaderPathFile("transformed.png", output, stat)
+	if err != nil {
+		return nil, err
+	}
+
+	return outnode, nil
+}
+
+func saveLink(oldpath, newpath string) error {
+	linknode, err := os.OpenFile("linkstore", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0664)
+	if err != nil {
+		return err
+	}
+	defer linknode.Close()
+
+	listfile, err := os.ReadFile("linkstore")
+	if err != nil {
+		return err
+	}
+
+	list := string(listfile)
+	lines := strings.Split(list, "\n")
+
+	for _, v := range lines {
+		if oldpath+":"+newpath == v {
+			return nil
+		}
+	}
+
+	_, err = linknode.WriteString(oldpath + ":" + newpath + "\n")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getNode(ctx context.Context, api *CoreAPI, c cid.Cid) (ipld.Node, error) {
+	node, err := api.dag.Get(ctx, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return node, nil
+}
+
+func resizeNode(ctx context.Context, api *CoreAPI, node ipld.Node, param string) (ipld.Node, error) {
+	node, err := createNewImageNode(ctx, api, node, param)
+	if err != nil {
+		return nil, err
+	}
+
+	return node, nil
+}
+
+// use each cid and get original and resized node in parallel
+func getOriginalAndResizeInParallel(ctx context.Context, api *CoreAPI, original cid.Cid, resize cid.Cid) (chan ipld.Node, chan error) {
+	nodech := make(chan ipld.Node)
+	errch := make(chan error)
+
+	go func() {
+		param := original.GetParam()
+		original.SetParam("")
+
+		node, err := getNode(ctx, api, original)
+		if err != nil {
+			errch <- err
+		} else {
+			node, err = resizeNode(ctx, api, node, param)
+			if err != nil {
+				errch <- err
+			} else {
+				nodech <- node
+			}
+		}
+	}()
+
+	go func() {
+		node, err := getNode(ctx, api, resize)
+		if err != nil {
+			errch <- err
+		} else {
+			nodech <- node
+		}
+	}()
+
+	return nodech, errch
+}
+
+// get node in parallel
+// one is search with parameter, other is without parameter
+func getNodeParallel(ctx context.Context, api *CoreAPI, c cid.Cid) (chan ipld.Node, chan error) {
+	nodech := make(chan ipld.Node)
+	errch := make(chan error)
+
+	go func() {
+		param := c.GetParam()
+		c.SetParam("")
+
+		node, err := getNode(ctx, api, c)
+		if err != nil {
+			errch <- err
+		} else {
+			node, err = resizeNode(ctx, api, node, param)
+			if err != nil {
+				errch <- err
+			} else {
+				nodech <- node
+			}
+		}
+	}()
+
+	go func() {
+		node, err := getNode(ctx, api, c)
+		if err != nil {
+			errch <- err
+		} else {
+			nodech <- node
+		}
+	}()
+
+	return nodech, errch
 }
--- go.mod
+++ go.mod
@@ -243,6 +243,7 @@ require (
 	go.uber.org/multierr v1.8.0 // indirect
 	go4.org v0.0.0-20200411211856-f5505b9728dd // indirect
 	golang.org/x/exp v0.0.0-20220426173459-3bcf042a4bf5 // indirect
+	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
@@ -259,6 +260,11 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.1.7 // indirect
+)
+
+replace (
+	github.com/ipfs/go-bitswap v0.9.0 => ../../yoshikawa0711/go-bitswap
+	github.com/ipfs/go-cid v0.2.0 => ../../yoshikawa0711/go-cid
 )
 
 go 1.17
